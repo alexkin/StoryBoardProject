c930e53c3f634c4d322b4faf2797118a5b8c815d
ming <user@usermatoMacBook-Air.local>
2013-05-29 17:08:27 +0800

    Initial Commit

---
+++ StoryBoardProject/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StoryBoardProject
+//
+//  Created by ming on 13-5-29.
+//  Copyright (c) 2013年 ming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ StoryBoardProject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  StoryBoardProject
+//
+//  Created by ming on 13-5-29.
+//  Copyright (c) 2013年 ming. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
